7bec2994a394a01c1cc7751f1cb65aecf474f27c
donnicholaus@gmail.com <gaetanamD13>
2020-09-18 13:34:17 +0300

    Welcome activity initialixed

--- app/src/main/java/com/team/green/Welcome.java
+++ app/src/main/java/com/team/green/Welcome.java
@@ -2,7 +2,11 @@ package com.team.green;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.MediaParser;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Welcome extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class Welcome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        Button btnLogin = findViewById(R.id.loginBtn);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+            }
+        });
     }
 }
