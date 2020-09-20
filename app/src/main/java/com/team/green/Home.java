2c59b8e36b789d9b023cce8cff376cb770b47f85
donnicholaus@gmail.com <gaetanamD13>
2020-09-20 16:46:36 +0300

    Added subscription page

--- app/src/main/java/com/team/green/Home.java
+++ app/src/main/java/com/team/green/Home.java
@@ -2,13 +2,28 @@ package com.team.green;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Home extends AppCompatActivity {
+
+    Button btnToSub;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnToSub = findViewById(R.id.toSub);
+
+        btnToSub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Home.this, Subscription.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/team/green/Login.java
+++ app/src/main/java/com/team/green/Login.java
@@ -2,13 +2,29 @@ package com.team.green;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+
+    Button btnToSub;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_loigin);
+        setContentView(R.layout.activity_login);
+
+
+        btnToSub = findViewById(R.id.toSub);
+
+        btnToSub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login.this, Subscription.class);
+                startActivity(intent);
+            }
+        });
     }
 }
