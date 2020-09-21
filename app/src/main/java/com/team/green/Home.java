1c548795956cd847919ad1224caa78ad7bd162f7
donnicholaus@gmail.com <gaetanamD13>
2020-09-21 20:33:56 +0300

    Navigation Drawer added with its fragments

--- app/src/main/java/com/team/green/Home.java
+++ app/src/main/java/com/team/green/Home.java
@@ -1,24 +1,31 @@
 package com.team.green;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.view.Gravity;
+import android.view.MenuItem;
 import android.widget.Button;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class Home extends AppCompatActivity {
+public class Home extends AppCompatActivity implements  NavigationView.OnNavigationItemSelectedListener{
 
     Button btnToSub;
     DrawerLayout drawerLayout;
     Toolbar toolbar;
     NavigationView navigationView;
     ActionBarDrawerToggle actionBarDrawerToggle;
+
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,8 +38,17 @@ public class Home extends AppCompatActivity {
         navigationView = findViewById(R.id.navigation_view);
         actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        navigationView.setNavigationItemSelectedListener(this);
+
         actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
         actionBarDrawerToggle.syncState();
+
+
+        //Load default fragment
+        fragmentManager = getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.container_fragment, new Main());
+        fragmentTransaction.commit();
 
         btnToSub = findViewById(R.id.toSub);
 
@@ -43,5 +59,44 @@ public class Home extends AppCompatActivity {
 //                startActivity(intent);
 //            }
 //        });
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        drawerLayout.closeDrawer(GravityCompat.START);
+
+        switch(item.getItemId()){
+
+            case R.id.home:
+                fragmentManager = getSupportFragmentManager();
+                fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.container_fragment, new Main());
+                fragmentTransaction.commit();
+                break;
+
+            case R.id.profile:
+                fragmentManager = getSupportFragmentManager();
+                fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.container_fragment, new Profile());
+                fragmentTransaction.commit();
+                break;
+
+            case R.id.settings:
+
+                fragmentManager = getSupportFragmentManager();
+                fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.container_fragment, new Settings());
+                fragmentTransaction.commit();
+                break;
+
+            case R.id.about:
+                fragmentManager = getSupportFragmentManager();
+                fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.container_fragment, new About());
+                fragmentTransaction.commit();
+                break;
+        }
+
+        return false;
     }
 }
