9420c7e3cb003ba4b38c1612b5ec7f10a33463f4
tiffah <tiffahkapiligi@gmail.com>
2020-09-22 14:59:45 +0300

    }

--- app/src/main/java/com/team/green/Main.java
+++ app/src/main/java/com/team/green/Main.java
@@ -1,5 +1,6 @@
 package com.team.green;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,13 +8,35 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+import java.util.zip.Inflater;
 
 public class Main extends Fragment {
+
+    View view;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        RelativeLayout cb = view.findViewById(R.id.cleaningbox);
+
+        cb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), Cleaning.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
     }
-}
+
+    }
+
