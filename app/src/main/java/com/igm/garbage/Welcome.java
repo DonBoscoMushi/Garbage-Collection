4830d625fb797483deeba7503f34afbe35d0f46d
ibrahimgoodluck <ibrah.goodluck@gmail.com>
2021-06-14 18:00:31 +0300

    changed activity names

--- app/src/main/java/com/igm/garbage/Welcome.java
+++ app/src/main/java/com/igm/garbage/Welcome.java
@@ -12,7 +12,7 @@ public class Welcome extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome);
+
 
         Handler handler = new Handler(Looper.getMainLooper());
 
